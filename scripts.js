b1ebc50843a373e35e960d2fe0b022aa3360afa4
duckingwood <duckingwood@gmail.com>
2019-10-17 18:37:47 -0500

    Update formula for In2Ft & BMI

--- scripts.js
+++ scripts.js
@@ -1,25 +1,22 @@
 // solo project calculator
 
 const calculator = {
-  add(a, b) {
-    return Number(a) + Number(b);
+BMI(a, b) {
+    return (a/(b * b)) * 703;
   },
-  subtract(a, b) {
-    return a - b;
-  },
-  multiply(a, b) {
-    return a * b;
-  },
-  divide(a, b) {
-    return a / b;
+  In2Ft(a, b) {
+    if (b > 0 || 0 < b) {
+      alert("invalid entry in second box, box should be set to 0")
+    }else
+    return (a/12); 
   }
 };
 
 console.log();
 
 const form = document.querySelector("form");
-const num1 = document.querySelector("#num-1");
-const num2 = document.querySelector("#num-2");
+const num1 = document.querySelector("#w");
+const num2 = document.querySelector("#h");
 const select = document.querySelector("select");
 const output = document.querySelector("#output");
 
@@ -33,7 +30,7 @@ form.addEventListener("submit", function(event) {
 
 /* Found this on codepen.io & thought it was really
 cool & would make the page jump out. It was also interesting to see
-the things we can do with javascript. lines 4 the code are 39 - 138 or everything below
+the things we can do with javascript. lines 4 the code are 36 - 135 or everything below
 https://codepen.io/strangerintheq (css designer)
 https://codepen.io/strangerintheq/pen/XWrwjQv (actual pen) */
 let rnd = n => (Math.random()-0.5)*(n||1);
@@ -115,7 +112,7 @@ function growIteration(){
     if (g.length) 
         requestAnimationFrame(growIteration);
     else grow(0,0)
-    growStep(111)
+    growStep(output.textContent) //input method that takes output of number and changes grow interation
 }
 function paintSegment(s){
   ctx.lineWidth = s.width;
